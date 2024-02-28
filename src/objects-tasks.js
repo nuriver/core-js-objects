15896d2ef064f3155257ece007c2250184b69905
Aleksei Golubkov <nuriver@gmail.com>
2024-02-28 21:33:23 +0100

    feat: finish sellTickets task

--- src/objects-tasks.js
+++ src/objects-tasks.js
@@ -164,8 +164,18 @@ function makeWord(lettersObject) {
  *    sellTickets([25, 25, 50]) => true
  *    sellTickets([25, 100]) => false (The seller does not have enough money to give change.)
  */
-function sellTickets(/* queue */) {
-  throw new Error('Not implemented');
+function sellTickets(queue) {
+  if (queue.length === 0) return true;
+  let sum = 0;
+  for (let i = 0; i < queue.length; i += 1) {
+    if (queue[i] === 25) sum += queue[i];
+    if (queue[i] > 25) {
+      if (queue[i] - sum > 25) {
+        return false;
+      }
+    }
+  }
+  return true;
 }
 
 /**
